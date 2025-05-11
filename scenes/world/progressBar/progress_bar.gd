ff42dc61652796aced001ba4ea467731060b2d30
Molie Robert <molie.robert@ubisoft.com>
2025-05-11 17:43:05 -0400

    add a working progress bar to rover

---
+++ scenes/world/progressBar/progress_bar.gd
@@ -0,0 +1,33 @@
+class_name Juicy_bar extends Control
+
+@export var top_layer_bar: ProgressBar
+@export var bottom_layer_bar : ProgressBar
+
+@export var min_value :float
+@export var max_value: float
+@export var current_value: float
+
+func _ready() -> void:
+	current_value = max_value
+	set_progress_bar_default_values(top_layer_bar)
+	set_progress_bar_default_values(bottom_layer_bar)
+	pass # Replace with function body.
+	
+func set_progress_bar_default_values(bar:ProgressBar):
+	bar.min_value = min_value
+	bar.max_value = max_value
+	bar.value = current_value
+	pass
+	
+func change_current_value(value: float):
+	current_value = clamp(value, min_value, max_value)
+	run_juicy_tween(top_layer_bar, current_value, 0.2,0)
+	run_juicy_tween(bottom_layer_bar, current_value, 0.4,0.1 )
+	pass
+	
+func run_juicy_tween(bar: ProgressBar, value: float, lenght: float, delay: float):
+	var tween = get_tree().create_tween()
+	tween.tween_property(bar, "value" , value, lenght).set_delay(delay)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scenes/world/rover/vehicle_body_3d.gd
+++ scenes/world/rover/vehicle_body_3d.gd
@@ -3,11 +3,27 @@ extends VehicleBody3D
 @export var MAX_STEER = 0.9
 @export var ENGINE_POWER = 300
 var triggerRecover = false
+var progress_bar: Juicy_bar
 
+func _ready():
+
+	progress_bar = get_node("../../CanvasGroup/juicyBar")
+
+	if progress_bar == null:
+		print("Progress bar not initialized!")
+	else: 
+		print("Progress bar initialized successfully!")
+
+	
 func _physics_process(delta):
 	var s = Input.get_axis("move_right", "move_left")
 	steering = move_toward(steering,  s * MAX_STEER, delta * 10)
 	engine_force = Input.get_axis("move_down", "move_up") * ENGINE_POWER
+
+	if engine_force > 0:
+		progress_bar.change_current_value(progress_bar.current_value - 1)
+
+
 	
 func _integrate_forces(state):
 	#https://gamedevacademy.org/physicsdirectbodystate3d-in-godot-complete-guide/
