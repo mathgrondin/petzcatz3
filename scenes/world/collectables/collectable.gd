66ae6511da6b1dacccf890507913f892c079e7d7
philou <117863579+philoufc@users.noreply.github.com>
2025-05-18 01:12:14 -0400

    upgrade collectable

--- scenes/world/collectables/collectable.gd
+++ scenes/world/collectables/collectable.gd
@@ -1,17 +1,25 @@
 extends Area3D
 
+@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+const ALGUE_DETAIL = preload("res://assets/UI/algue_detail.png")
+
 #const ROT_SPEED = 2
 @export var value: int = 1
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export_enum("red", "blue") var collectable_type: String:
+	set(new_type):
+		collectable_type = new_type
+		match collectable_type:
+			"red":
+				Color(1.0, 0.23, 0.23)
+			"blue":
+				Color(0.0, 0.333, 1.0)
+			_:
+				Color(1.0, 1.0, 1.0)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	#rotate_y(deg_to_rad(ROT_SPEED))
-	pass 
-"res://scripts/game_controller.gd"
+func colorize(color: Color) -> void:
+	mesh_instance_3d.mesh.get_active_material(0).albedo_color = color
+
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Node3D:
