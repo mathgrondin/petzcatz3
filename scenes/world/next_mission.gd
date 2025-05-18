0254de21d5b0d3b0862c9fca1743c93011b36108
Molie Robert <molie.robert@ubisoft.com>
2025-05-18 10:54:27 -0400

    button is working again!

--- scenes/world/next_mission.gd
+++ scenes/world/next_mission.gd
@@ -8,6 +8,4 @@ func _ready():
 
 
 func _on_texture_button_pressed() -> void:
-	var next_mission_scene = load("res://scenes/NextMission.tscn")
-	var next_mission_instance = next_mission_scene.instantiate()
-	get_tree().current_scene.add_child(next_mission_instance)
+	get_tree().change_scene_to_file("res://scenes/world/world.tscn")
