aee6af0123749dd855fcb448976cabdd2e8700c0
philou <117863579+philoufc@users.noreply.github.com>
2025-05-18 13:21:47 -0400

    collectable work-in-progress

--- scenes/world/collectables/collectable.gd
+++ scenes/world/collectables/collectable.gd
@@ -1,28 +1,32 @@
 extends Area3D
 
-@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@onready var collectable_mesh_instance_3d: MeshInstance3D = $CollectableMeshInstance3D
 const ALGUE_DETAIL = preload("res://assets/UI/algue_detail.png")
+const ALGUE_COLORS = {
+	"red":
+		Color(1.0, 0.23, 0.23),
+	"blue":
+		Color(0.0, 0.333, 1.0),
+	"white":
+		Color(1.0, 1.0, 1.0),
+}
 
 #const ROT_SPEED = 2
 @export var value: int = 1
 @export_enum("red", "blue") var collectable_type: String:
 	set(new_type):
 		collectable_type = new_type
-		match collectable_type:
-			"red":
-				Color(1.0, 0.23, 0.23)
-			"blue":
-				Color(0.0, 0.333, 1.0)
-			_:
-				Color(1.0, 1.0, 1.0)
 
+func _ready() -> void:
+	await get_tree().process_frame
+	colorize(ALGUE_COLORS[collectable_type])
 
 func colorize(color: Color) -> void:
-	mesh_instance_3d.mesh.get_active_material(0).albedo_color = color
-
+	collectable_mesh_instance_3d.mesh.material.albedo_color = color
 
 func _on_body_entered(body: Node3D) -> void:
-	if body is Node3D:
+	if body is Node3D and body.name == "VehicleBody3D":
+		print(body.name)
 		GameController.collectable_collected(value)
 		self.queue_free() # Replace with function body.
 		print("collect 1")
